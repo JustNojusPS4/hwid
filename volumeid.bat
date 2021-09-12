d515e4dfbb64bf288db593a83d108cb406d40e00
JustNojusPS4 <44664356+JustNojusPS4@users.noreply.github.com>
2021-09-12 12:15:19 +0300

    Add files via upload

---
+++ volumeid.bat
@@ -0,0 +1,13 @@
+@echo off
+cd \
+mkdir VolumeID
+cd \VolumeID\
+:loop
+set rnd=%random%
+if %rnd% GTR 9999 goto loop
+if %rnd% LSS 1000 goto loop
+set rnd1=%random%
+if %rnd1% GTR 9999 goto loop
+if %rnd1% LSS 1000 goto loop
+volumeid C: %rnd%-%rnd1%
+pause
